f98a17f36417576da64be06fbfdf9b75d6de6934
AaronPenner <rockcairncom@gmail.com>
2025-06-25 13:34:16 -0600

    changes for mysql2 use

--- README.md
+++ README.md
@@ -33,6 +33,18 @@
    - that references your already created mysql database.
    - > _there are many ways to install mysql and I am using my instance from my
      > https://www.apachefriends.org/ XXAMP install which I use for PHP development._
+   - MacOs >  _Homebrew_ - install information (this will require mysql2 package for auth)
+      ```
+      brew install mysql
+      brew services start mysql
+      
+      mysql -u root
+      CREATE DATABASE javascriptdb;
+      CREATE USER 'jstest'@'localhost' IDENTIFIED BY 'password';
+      GRANT ALL PRIVILEGES ON javascriptdb.* TO 'jstest'@'localhost' WITH GRANT OPTION;
+      
+      brew services stop mysql
+      ```
 1. Adjust the _flyway.cjs_ file to match your connection information
 1. Create Tables and Seed your database with flyway
    - `pnpm migrate`
--- src/server/knexconfig.ts
+++ src/server/knexconfig.ts
@@ -2,16 +2,17 @@ import fastify, { FastifyInstance } from 'fastify';
 import knex from 'knex';
 import { Envs } from '../server.js';
 
-
-export default function databaseConnection(fastify: FastifyInstance): knex.Knex<any, unknown[]> {
-    const envs = fastify.getEnvs<Envs>();
-    return knex({
-        client: 'mysql',
-        connection: {
-          host: envs.DB_HOST,
-          user: envs.DB_USERNAME,
-          password: envs.DB_PASSWORD,
-          database: envs.DB_DATABASE,
-        },
-      });
-    }
+export default function databaseConnection(
+  fastify: FastifyInstance
+): knex.Knex<any, unknown[]> {
+  const envs = fastify.getEnvs<Envs>();
+  return knex({
+    client: 'mysql2',
+    connection: {
+      host: envs.DB_HOST,
+      user: envs.DB_USERNAME,
+      password: envs.DB_PASSWORD,
+      database: envs.DB_DATABASE,
+    },
+  });
+}
